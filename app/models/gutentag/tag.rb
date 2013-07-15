b491a6ae5e78b2b324baa830833a9f40ae77b1c1
Pat Allan <pat@freelancing-gods.com>
2013-07-15 16:02:20 +1000

    Save tags with normalised names.

--- app/models/gutentag/tag.rb
+++ app/models/gutentag/tag.rb
@@ -3,4 +3,12 @@ class Gutentag::Tag < ActiveRecord::Base
     :dependent => :destroy
 
   validates :name, :presence => true, :uniqueness => {:case_sensitive => false}
+
+  before_validation :normalise_name
+
+  private
+
+  def normalise_name
+    self.name = Gutentag::TagName.normalise name
+  end
 end
--- spec/models/gutentag/tag_spec.rb
+++ spec/models/gutentag/tag_spec.rb
@@ -1,6 +1,23 @@
 require 'spec_helper'
 
 describe Gutentag::Tag do
+  describe '#name' do
+    before :each do
+      Gutentag::TagName.stub :normalise => 'waffles'
+    end
+
+    it "normalises the provided name" do
+      Gutentag::TagName.should_receive(:normalise).with('Pancakes').
+        and_return('waffles')
+
+      Gutentag::Tag.create!(:name => 'Pancakes')
+    end
+
+    it "saves the normalised name" do
+      Gutentag::Tag.create!(:name => 'Pancakes').name.should == 'waffles'
+    end
+  end
+
   describe '#valid?' do
     it "ignores case when enforcing uniqueness" do
       Gutentag::Tag.create! :name => 'pancakes'
