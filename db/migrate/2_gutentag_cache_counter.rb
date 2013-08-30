105c9b9e05feaf96ae6eff3fe7f0ea1fd30d8478
Nathan S <nathan@inspire9.com>
2013-08-30 21:37:50 +1000

    update migration to reset existing cache

--- db/migrate/2_gutentag_cache_counter.rb
+++ db/migrate/2_gutentag_cache_counter.rb
@@ -1,8 +1,12 @@
 class GutentagCacheCounter < ActiveRecord::Migration
   def up
     add_column :tags, :taggings_count, :integer, default: 0
-
     add_index :tags, :taggings_count
+
+    Gutentag::Tag.reset_column_information
+    Gutentag::Tag.select(:id).collect(&:id).each do |tag_id|
+      Gutentag::Tag.reset_counters tag_id, :taggings
+    end
   end
 
   def down
