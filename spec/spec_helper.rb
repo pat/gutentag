e7731acac18089fc44f84689ebcad23abedda62c
Pat Allan <pat@freelancing-gods.com>
2017-01-19 20:56:19 +1100

    Use `use_transactional_tests` if it exists.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,5 +7,9 @@ Combustion.initialize! :active_record
 require 'rspec/rails'
 
 RSpec.configure do |config|
-  config.use_transactional_fixtures = true
+  if config.respond_to?(:use_transactional_tests)
+    config.use_transactional_tests = true
+  else
+    config.use_transactional_fixtures = true
+  end
 end
