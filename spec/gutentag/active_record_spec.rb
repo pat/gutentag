db5c314896add573958e8a9727cf15111c189147
Pat Allan <pat@freelancing-gods.com>
2017-05-16 11:21:51 +1000

    Fix Melbourne typos.

--- spec/gutentag/active_record_spec.rb
+++ spec/gutentag/active_record_spec.rb
@@ -5,9 +5,9 @@ SingleCov.covered! file: "lib/gutentag/tagged_with_query.rb"
 
 describe Gutentag::ActiveRecord do
   describe '.tagged_with' do
-    let!(:melborne_article) do
+    let!(:melbourne_article) do
       article = Article.create :title => 'Overview'
-      article.tag_names << 'melborne'
+      article.tag_names << 'melbourne'
       article.save!
       article
     end
@@ -19,83 +19,83 @@ describe Gutentag::ActiveRecord do
       article
     end
 
-    let!(:melborne_oregon_article) do
+    let!(:melbourne_oregon_article) do
       article = Article.create
-      article.tag_names = %w(oregon melborne)
+      article.tag_names = %w(oregon melbourne)
       article.save!
       article
     end
 
     context 'given a single tag name' do
-      subject { Article.tagged_with('melborne') }
+      subject { Article.tagged_with('melbourne') }
 
       it { expect(subject.count).to eq 2 }
-      it { is_expected.to include melborne_article, melborne_oregon_article }
+      it { is_expected.to include melbourne_article, melbourne_oregon_article }
       it { is_expected.not_to include oregon_article }
     end
 
     context 'given a single tag name[symbol]' do
-      subject { Article.tagged_with(:melborne) }
+      subject { Article.tagged_with(:melbourne) }
 
       it { expect(subject.count).to eq 2 }
-      it { is_expected.to include melborne_article, melborne_oregon_article }
+      it { is_expected.to include melbourne_article, melbourne_oregon_article }
       it { is_expected.not_to include oregon_article }
     end
 
     context 'given a denormalized tag name' do
-      subject { Article.tagged_with("MelbornE") }
+      subject { Article.tagged_with("MelbournE") }
 
       it { expect(subject.count).to eq 2 }
-      it { is_expected.to include melborne_article, melborne_oregon_article }
+      it { is_expected.to include melbourne_article, melbourne_oregon_article }
       it { is_expected.not_to include oregon_article }
     end
 
     context 'given multiple tag names' do
-      subject { Article.tagged_with('melborne', 'oregon') }
+      subject { Article.tagged_with('melbourne', 'oregon') }
 
       it { expect(subject.count).to eq 3 }
-      it { is_expected.to include melborne_article, oregon_article, melborne_oregon_article }
+      it { is_expected.to include melbourne_article, oregon_article, melbourne_oregon_article }
     end
 
     context 'given an array of tag names' do
-      subject { Article.tagged_with(%w(melborne oregon)) }
+      subject { Article.tagged_with(%w(melbourne oregon)) }
 
       it { expect(subject.count).to eq 3 }
-      it { is_expected.to include melborne_article, oregon_article, melborne_oregon_article }
+      it { is_expected.to include melbourne_article, oregon_article, melbourne_oregon_article }
     end
 
     context 'given a single tag instance' do
-      subject { Article.tagged_with(Gutentag::Tag.find_by_name!('melborne')) }
+      subject { Article.tagged_with(Gutentag::Tag.find_by_name!('melbourne')) }
 
       it { expect(subject.count).to eq 2 }
-      it { is_expected.to include melborne_article, melborne_oregon_article }
+      it { is_expected.to include melbourne_article, melbourne_oregon_article }
       it { is_expected.not_to include oregon_article }
       it { expect(subject.to_sql).not_to include 'gutentag_tags' }
     end
 
     context 'given a single tag id' do
-      subject { Article.tagged_with(Gutentag::Tag.find_by_name!('melborne').id) }
+      subject { Article.tagged_with(Gutentag::Tag.find_by_name!('melbourne').id) }
 
       it { expect(subject.count).to eq 2 }
-      it { is_expected.to include melborne_article, melborne_oregon_article }
+      it { is_expected.to include melbourne_article, melbourne_oregon_article }
       it { is_expected.not_to include oregon_article }
       it { expect(subject.to_sql).not_to include 'gutentag_tags' }
     end
 
     context 'given multiple tag objects' do
-      subject { Article.tagged_with(Gutentag::Tag.where(name: %w(melborne oregon))) }
+      subject { Article.tagged_with(Gutentag::Tag.where(name: %w(melbourne oregon))) }
 
       it { expect(subject.count).to eq 3 }
-      it { is_expected.to include melborne_article, oregon_article, melborne_oregon_article }
+      it { is_expected.to include melbourne_article, oregon_article, melbourne_oregon_article }
       it { expect(subject.to_sql).not_to include 'gutentag_tags' }
     end
 
     context 'chaining where clause' do
-      subject { Article.tagged_with(%w(melborne oregon)).where(title: 'Overview') }
+      subject { Article.tagged_with(%w(melbourne oregon)).where(title: 'Overview') }
 
       it { expect(subject.count).to eq 1 }
-      it { is_expected.to include melborne_article }
-      it { is_expected.not_to include oregon_article, melborne_oregon_article }
+      it { is_expected.to include melbourne_article }
+      it { is_expected.not_to include oregon_article, melbourne_oregon_article }
     end
   end
 end
