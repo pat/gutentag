fcbe2747f7cec0dcd88d04d113edc7aa83b70bb1
Pat Allan <pat@freelancing-gods.com>
2017-06-02 22:47:08 +1000

    More verbose, but I think I prefer this approach.

--- lib/gutentag/tagged_with.rb
+++ lib/gutentag/tagged_with.rb
@@ -1,12 +1,34 @@
 class Gutentag::TaggedWith
   def self.call(model, options)
-    match = options[:match] || :any
-    if options[:ids]
-      IDQuery.new(model, options[:ids], match).call
-    elsif options[:tags]
-      IDQuery.new(model, Array(options[:tags]).collect(&:id), match).call
+    new(model, options).call
+  end
+
+  def initialize(model, options)
+    @model   = model
+    @options = options
+  end
+
+  def call
+    query_class.new(model, values, match).call
+  end
+
+  private
+
+  attr_reader :model, :options
+
+  def match
+    options[:match] || :any
+  end
+
+  def query_class
+    options[:names] ? NameQuery : IDQuery
+  end
+
+  def values
+    if options[:tags]
+      Array(options[:tags]).collect(&:id)
     else
-      NameQuery.new(model, options[:names], match).call
+      options[:ids] || options[:names]
     end
   end
 end
